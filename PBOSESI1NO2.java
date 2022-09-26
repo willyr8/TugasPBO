868888d5f220528b5f40e43a61b03e2a21a6e073
willyr8 <51260855+willyr8@users.noreply.github.com>
2022-09-26 21:55:03 +0700

    Add files via upload

---
+++ PBOSESI1NO2.java
@@ -0,0 +1,14 @@
+
+package pbosesi1no2;
+import javax.swing.JOptionPane;
+
+
+public class PBOSESI1NO2 {
+public static void main(String[] args) {
+ String nama ="";
+        nama = JOptionPane.showInputDialog("Anda Sedang Belajar Apa ?");
+        String pesan = ( "belajar " + nama + " sangat mudah");
+        JOptionPane.showMessageDialog(null, pesan);               
+    }
+    
+}
