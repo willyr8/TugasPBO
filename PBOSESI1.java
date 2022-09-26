baf73641e175de0c6758a94748c1515a793fdaa6
willyr8 <51260855+willyr8@users.noreply.github.com>
2022-09-26 21:24:37 +0700

    Add files via upload

---
+++ PBOSESI1.java
@@ -0,0 +1,12 @@
+package pbosesi1;
+public class PBOSESI1 {
+
+
+    public static void main(String[] args) {
+         System.out.println("Belajar JAVA");
+         System.out.println("Sangat Mudah Sekali");
+        System.out.println("dan sangat menyenangkan");
+      
+    }
+    
+}
